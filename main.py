9fc77bbd08c68d71f4950cad78e7d0060c7002ab
Jürgen Malinowski <jm-malle@gmx.de>
2026-03-08 20:45:17 +0100

    feat IMPORTS

--- main.py
+++ main.py
@@ -3,16 +3,39 @@
 # Thanks für Icon Discord "TestServerPyBot"
 # <a href="https://www.flaticon.com/de/kostenlose-icons/gruppenchat" title="gruppenchat Icons">Gruppenchat Icons erstellt von Mehwish - Flaticon</a>
 
-# aus dotenv nun die Funktion "load_dotenv" importieren/loaden ...
-from dotenv import load_dotenv
+# Discord Funktionalitäten importieren ...
+import discord
+
 # nun op importieren, Windows-Befehlssatz für Dateioperationen
 import os
+
+# aus dotenv nun die Funktion "load_dotenv" importieren...
+from dotenv import load_dotenv
 
 # lädt die Variablen aus .env in die Umgebungsvariablen des Projekts
 # zur späteren Nutzung ...
 load_dotenv()  
 
-# den persönlichen KEY aus Datei .env nun in die Variable "secretKey"
-# einlesen und prüfen mit print() ...pi
-secretKey = os.getenv("SECRET_KEY")
-print(secretKey)
+# Den Discord-Token aus der .env laden ...
+token = os.getenv("DISCORD_TOKEN")
+
+
+
+
+
+
+
+"""
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('my token goes here')
+"""
