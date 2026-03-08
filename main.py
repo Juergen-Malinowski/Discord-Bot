0e8b474bf5ab0d81dd2b6012a755417f587dc626
Jürgen Malinowski <jm-malle@gmx.de>
2026-03-08 22:08:11 +0100

    FEAT:  Discord-Bot now running

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Discord Funktionalitäten importieren ...
 import discord
+# Import der Discord-commands zur Nutzung ...
+from discord.ext import commands
 
 # nun op importieren, Windows-Befehlssatz für Dateioperationen
 import os
@@ -16,10 +18,21 @@ load_dotenv()
 # Den Discord-Token aus der .env laden ...
 token = os.getenv("DISCORD_TOKEN")
 
+# Dem Bot die Erlaubnis / Regeln aus Dicord übergeben, die default().
+# Diese habe ich in Discord bei Erstellung des Bots festgelegt, sind also änderbar ...
+intents = discord.Intents.default()
 
+# Der "prefix", hier definiert als "!" leitet später in Discord einen Befehl an den Bot
+# ein, damit der Bot die Nachricht als Befehl (Funktions-Aufruf) an ihn interpretiert.
+# Mit intents=intents(Variable oben) weisen wir dem Bot die default()-Befehle zur Nutzung zu.
+bot = commands.Bot(command_prefix="!", intents=intents)
 
+@bot.event
+async def on_ready():
+    print(f"Ready !!! Ich bin {bot.user}")
 
-
+# Den bot starten und mit der Übergabe des token authentifizieren ...
+bot.run(token)
 
 
 """
