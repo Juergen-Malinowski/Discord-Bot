db798315a6bce99ad2e4f855109dbf0fb68cfd74
Jürgen Malinowski <jm-malle@gmx.de>
2026-03-08 11:38:53 +0100

    Create a ‘Discord bot’ project – Step 2

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 # Hauptprogramm vom Discord-Bot
-#
-# aus dotenv nun die Funktion "dotenv" importieren/loaden ...
+
+# aus dotenv nun die Funktion "load_dotenv" importieren/loaden ...
 from dotenv import load_dotenv
+# nun op importieren, Windows-Befehlssatz für Dateioperationen
+import os
+
+# lädt die Variablen aus .env in die Umgebungsvariablen des Projekts
+# zur späteren Nutzung ...
+load_dotenv()  
+
+secretKey = os.getenv("SECRET_KEY")
+print(secretKey)
